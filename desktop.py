8d19e759574350d9db1ae031ca9b9e3f89267349
Jiun Kim <jkim2@bowdoin.edu>
2020-08-18 01:16:06 +0900

    enable and disable button

--- desktop.py
+++ desktop.py
@@ -31,6 +31,20 @@ class BaseWindow:
         entry.pack()
         return entry
 
+    @staticmethod
+    def _disable(button=None, label=None):
+        if button:
+            button.config(state='disable')
+        if label:
+            label.config(state='disable')
+
+    @staticmethod
+    def _enable(button=None, label=None):
+        if button:
+            button.config(state='normal')
+        if label:
+            label.config(state='normal')
+
 
 class LandingWindow(BaseWindow):
     def __init__(self):
@@ -39,7 +53,7 @@ class LandingWindow(BaseWindow):
         self.root.geometry("400x400")
         self._add_label(text="Welcome to Marius Stocks", row=0, column=1, font=('Helvetica', 24))
         self.entry = self._add_entry(width=30, borderwidth=10, columnspan=3, row=1)
-        self._add_button(
+        self.find_stock_button = self._add_button(
             text="Go to Stocks",
             row=2,
             column=1,
@@ -48,90 +62,18 @@ class LandingWindow(BaseWindow):
         )
         self.root.mainloop()
 
-    def _command_find_stock(self, entry):
-        value = entry.get()
-        entry.delete(0, END)
-        if value == 'Alpaca Balance':
-            result = AlpacaView(AlpacaUseCase(AlpacaRepository())).get()
-            Label(self.root, text=result).pack()
+    def _command_find_stock(self, entry, button=None):
+        if self.find_stock_button['text'] == 'Go to Stocks':
+            value = entry.get()
+            entry.delete(0, END)
+            if value == 'Alpaca Balance':
+                result = AlpacaView(AlpacaUseCase(AlpacaRepository())).get()
+                Label(self.root, text=result).pack()
+            else:
+                result = AlpacaView(AlpacaUseCase(AlpacaRepository())).get_financial_statement(value)[0]
+                Label(self.root, text=json.dumps(result), wraplength=250).pack()
+            self.find_stock_button['text'] = 'Here it is'
         else:
-            result = AlpacaView(AlpacaUseCase(AlpacaRepository())).get_financial_statement(value)[0]
-            Label(self.root, text=json.dumps(result), wraplength=250).pack()
-
-    # def btn3func(self):
-    #     self.entry.state(['disabled'])
-# import tkinter as tk
-# from tkinter import ttk
-
-# class entryApp:
-
-#     def __init__(self, master):
-#         self.label = ttk.Label(master, text='Enter the text below')
-#         self.label.pack()
-        
-#         self.entry = ttk.Entry(master, width = 30)  # number of characters along the width
-#         self.entry.pack()
-        
-#         self.button = ttk.Button(master, text = "Get Entry")
-#         self.button.pack()
-#         self.tkstrvar = tk.StringVar()  # create tk string variable
-#         self.tkstrvar.set('Nothing is done yet!')   # set the value of tk string variable
-#         self.button.config(command = self.getEntry)
-        
-#         self.msg = ttk.Label(master, text = self.tkstrvar.get())    # get the value of string variable
-#         self.msg.pack()
-        
-#         self.btn1 = ttk.Button(master, text='Delete the entry', command = self.btn1func)
-#         self.btn1.pack()
-        
-#         self.crypt = tk.StringVar()
-#         self.crypt.set('Encrypt')
-#         self.btn2 = ttk.Button(master, text = "{} Text in Entry Field".format(self.crypt.get()), command = self.changecrypt)
-#         self.btn2.pack()
-#         #self.entryText = ttk.Entry(master, width=30)
-        
-#         ttk.Button(master, text = 'Disable Entry Field', command = self.btn3func).pack()
-#         ttk.Button(master, text = 'Enable Entry Field', command = self.btn4func).pack()
-#         ttk.Button(master, text = 'Readonly Entry Field', command = self.btn5func).pack()
-#         ttk.Button(master, text = 'Edit Entry Field', command = self.btn6func).pack()
-    
-#     def changecrypt(self):
-#         if self.crypt.get()=='Encrypt':
-#             self.entry.config(show='*')
-#             self.crypt.set('Decrypt')
-#             self.btn2.config(text = "{} Text in Entry Field".format(self.crypt.get()))
-#         else:
-#             self.entry.config(show='')
-#             self.crypt.set('Encrypt')
-#             self.btn2.config(text = "{} Text in Entry Field".format(self.crypt.get()))
-    
-#     def btn3func(self):
-#         self.entry.state(['disabled'])
-    
-#     def btn4func(self):
-#         self.entry.state(['!disabled'])
-        
-#     def btn5func(self):
-#         self.entry.state(['readonly'])
-        
-#     def getEntry(self):
-#         self.tkstrvar.set(self.entry.get())     # get entry widget content and store it in tk_string variable
-#         self.msg.config(text = self.tkstrvar.get())   # set msg as value of string variable
-        
-#     def btn1func(self):
-#         self.entry.delete(0, tk.END)    # delete all from 0 to END character in Entry Widget
-
-#     def btn6func(self):
-#         self.entry.state(['!readonly'])
-
-
-# def launchEntryApp():
-#     root = tk.Tk()
-#     entryApp(root)
-#     tk.mainloop()
-
-
-# def test():
-#     launchEntryApp()
-            
-# if __name__ == '__main__': test()
+            self._enable(button=self.find_stock_button)
+            entry.delete(0, END)
+            self.find_stock_button['text'] = 'Go to Stocks'
